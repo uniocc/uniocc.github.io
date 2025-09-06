954bf6b9b78a36dda8fd60e1d74b95873d948b07
Richard <72771268+richard-robotics@users.noreply.github.com>
2025-09-06 14:06:44 -0700

    Refactor webpage and add video

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
+var INTERP_BASE = "./fonts/interpolation/stacked";
 var NUM_INTERP_FRAMES = 240;
 
 var interp_images = [];
